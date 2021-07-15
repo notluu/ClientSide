55fc9ed6c18d154bdb646113fba8cf6d61e2d14c
notluu <79069323+notluu@users.noreply.github.com>
2021-07-15 22:21:56 +0800

    ClientSidemod

---
+++ ClientSide/src/main/java/com/notluu/clientside/ClientSide.java
@@ -0,0 +1,27 @@
+package com.notluu.clientside;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("clientside")
+public class ClientSide
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public ClientSide() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) { }
+
+    private void doClientStuff(final FMLClientSetupEvent event) { }
+}
+
